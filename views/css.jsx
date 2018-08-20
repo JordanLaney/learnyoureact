2df0ed36d56fbbcd55a2752890344033fcd5856b
JordanLaney <11jlaney11@gmail.com>
2018-08-20 14:47:11 -0700

    Retab

--- views/css.jsx
+++ views/css.jsx
@@ -2,69 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class TodoBox extends React.Component {
-	render() {
-		return (
-			<div className="todoBox">
-				<h1>Todos</h1>
-				<TodoList />
-				<TodoForm />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList />
+        <TodoForm />
+      </div>
+    );
+  }
 }
 
 class TodoList extends React.Component {
-	render() {
-		return(
-			<div className="todoList">
-				<table style={{border: "2px solid black;"}}>
-					<tbody>
-						<Todo title="Shopping">Milk</Todo>
-						<Todo title="Hair cut">13:00</Todo>
-						<Todo title="Learn React">15:00</Todo>
-					</tbody>
-				</table>
-			</div>
-		);
-	}	
+  render() {
+    return(
+      <div className="todoList">
+        <table style={{border: "2px solid black;"}}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+            <Todo title="Learn React">15:00</Todo>
+          </tbody>
+        </table>
+      </div>
+    );
+  } 
 }
 
 class Todo extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {checked: false};
-	}
-	handleChange(event) {
-		this.setState({checked: event.target.checked});
-	}
-	render() {
-		return (
-    	<tr>
-				<td style={style.tableContent}>
-					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
-				</td>
-      	<td style={style.tableContent}>{this.props.title}</td>
+  constructor(props) {
+    super(props);
+    this.state = {checked: false};
+  }
+  handleChange(event) {
+    this.setState({checked: event.target.checked});
+  }
+  render() {
+    return (
+      <tr>
+        <td style={style.tableContent}>
+          <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+        </td>
+        <td style={style.tableContent}>{this.props.title}</td>
         <td style={style.tableContent}>{this.props.children}</td>
       </tr>
-		);
-	}
+    );
+  }
 }
 Todo.propTypes = {
-	title: PropTypes.string
+  title: PropTypes.string
 }
 
 class TodoForm extends React.Component {
-	render() {
-		return (
-			<div className="todoForm">
-				I am a TodoForm.
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="todoForm">
+        I am a TodoForm.
+      </div>
+    );
+  }
 }
 
 let style = {
-	tableContent: {
-		border: "1px solid black;"
-	}
+  tableContent: {
+    border: "1px solid black;"
+  }
 }
